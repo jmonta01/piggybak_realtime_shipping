747c37caa9a0f0503a9f11095fcc60ad0f4cee8a
Tim Case <tim@powerupdev.com>
2012-10-05 23:34:09 +0700

    move destination, packages, weight into module

--- lib/piggybak_realtime_shipping/engine.rb
+++ lib/piggybak_realtime_shipping/engine.rb
@@ -3,6 +3,12 @@ module PiggybakRealtimeShipping
     isolate_namespace PiggybakRealtimeShipping
 
     require 'piggybak/config_decorator'    
+
+    initializer "piggy_back_realtime_shipping.include_on_piggybak_cart_and_order" do 
+      Piggybak::Order.send(:include, PiggybakRealtimeShipping)
+      Piggybak::Cart.send(:include, PiggybakRealtimeShipping)
+    end
+
     initializer "piggybak_realtime_shipping.add_calculators" do
       Piggybak.config do |config|
         config.shipping_calculators << "::Piggybak::ShippingCalculator::UpsShipping"
